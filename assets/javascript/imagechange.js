d2fc218d47952033fb785cf8c5c3b0a50e5b67fa
M3nt4llyS1lly <ggabriel.ishere@icloud.com>
2025-06-13 09:37:29 -0500

    Add files via upload

---
+++ assets/javascript/imagechange.js
@@ -0,0 +1,15 @@
+function Cleaning() {
+  document.getElementById('myImage').src = 'assets/images/Cleaning.jpg';
+}
+
+function Cooking() {
+  document.getElementById('myImage').src = 'assets/images/Cooking.jpg';
+}
+
+function Helping() {
+  document.getElementById('myImage').src = 'assets/images/Teaching.png';
+}
+
+function Talking() {
+  document.getElementById('myImage').src = 'assets/images/Talking1.png';
+}
